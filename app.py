d3db56d44231e84f180aca6808ec8f9766a075d0
Arijit Jana <62562067+arijitjana91@users.noreply.github.com>
2021-02-14 03:51:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+import sys
+from dbhelper import DBhelper
+class Flipkart:
+    def __init__(self):
+        # connect to the database
+        self.db = DBhelper()
+        self.menu()
+
+    def menu(self):
+
+        user_input = input("""
+        1. Enter 1 to register
+        2.Enter 2 to login
+        3. Anything else to leave
+        """)
+
+        if user_input == "1":
+            self.register()
+        elif user_input == "2":
+            self.login()
+        else:
+            sys.exit(1000)
+
+    def login_menu(self):
+        input("""
+        1. Enter 1 to see profile
+        2. enter 2 to edit profile
+        3. enter 3 to delete profile
+        4. enter 4 to logout
+        """)
+
+    def register(self):
+        name = input("Enter the name")
+        email = input("Enter the email")
+        password = input("Enter the password")
+
+        response = self.db.register(name,email,password)
+
+        if response:
+            print("Registration Successful")
+        else:
+            print("Registration Failed")
+
+        self.menu()
+
+    def login(self):
+        email = input("Enter email")
+        password = input("Enter password")
+
+        data = self.db.search(email,password)
+
+        if len(data) == 0:
+            print("Incorrect email/password")
+            self.menu()
+        else:
+            print("Hello",data[0][1])
+            self.login_menu()
+
+obj = Flipkart()
